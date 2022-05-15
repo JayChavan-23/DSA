866e6a1e5067de60139517a9facda0ae1c85cc47
JayChavan-23 <jaychavan2223@gmail.com>
2022-05-15 21:39:16 +0530

    first

---
+++ Arrays/printarrays.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class printarrays {
+    public static void main(String args[]){
+        String[] names = new String[5];
+        names[0]="Jay";
+        System.out.println(names[0]);
+        int numbers[]={1,5,8,4,6,6};
+        System.out.println(numbers[3]);
+    }
+}
