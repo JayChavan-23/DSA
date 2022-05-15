affcbdc099ad8b5d04869fb1ef4cffdce60813c5
JayChavan-23 <jaychavan2223@gmail.com>
2022-05-15 21:55:38 +0530

    first program

--- Arrays/printarrays.java
+++ Arrays/printarrays.java
@@ -7,5 +7,9 @@ public class printarrays {
         System.out.println(names[0]);
         int numbers[]={1,5,8,4,6,6};
         System.out.println(numbers[3]);
+        //always do n-1
+        for(int i = 0 ; i <= numbers.length-1;i++){
+            System.out.print(numbers[i]);
+        }
     }
 }
