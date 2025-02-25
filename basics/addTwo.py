50b7af47ed499698e5a16488996111980f0f817a
JayChavan-23 <jaychavan2223@gmail.com>
2025-02-25 22:09:49 +1030

    addTwo

---
+++ basics/addTwo.py
@@ -0,0 +1,24 @@
+def addition(a,b):
+    return a + b
+
+num1 = 8
+num2 = 8
+
+# Addition
+print(num1+num2)
+
+# Function
+
+print(addition(num2, num1))
+
+# Operator .add function
+
+import operator
+
+print(operator.add(num1, num2))
+
+# Lambda function
+
+n = lambda x,y: x+y
+
+print(n(2,3))
