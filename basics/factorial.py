243e3f8e9e8cf926737780ead11bdb7be86b6710
JayChavan-23 <jaychavan2223@gmail.com>
2025-02-26 22:32:29 +1030

    Factorial using recursion

--- basics/factorial.py
+++ basics/factorial.py
@@ -8,3 +8,10 @@ for i in range(1,num+1):
     fact*=i
 
 print(fact)
+
+#using recursive function
+
+def factorial(n):
+    return 1 if(n==1 or n==0) else n * factorial(n-1)
+
+print(factorial(num))
